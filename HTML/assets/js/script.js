66237f0c77fed500d96194e9760494472b8471b4
vAgyylka <168278168+vAgyylka@users.noreply.github.com>
2024-12-18 11:43:33 +0200

    Add files via upload

--- HTML/assets/js/script.js
+++ HTML/assets/js/script.js
@@ -1,3 +1,14 @@
 document.getElementById('theme-btn').addEventListener('click', () => {
     document.body.classList.toggle('dark');
 });
+// Determine score color
+let scoreColor;
+if (averageScore >= 7) {
+    scoreColor = "green";
+} else if (averageScore >= 4) {
+    scoreColor = "orange";
+} else {
+    scoreColor = "red";
+}
+
+
